25021200a15e15c488464e7e8297fbaa64cb216a
anatol1984 <anatol.kravchenko@gmail.com>
2018-10-15 18:15:00 +0300

    [SIM-2] external controller to change credentials (service URL)

--- app/code/local/Morozov/Similarity/controllers/IndexController.php
+++ app/code/local/Morozov/Similarity/controllers/IndexController.php
@@ -6,6 +6,25 @@ extends Mage_Core_Controller_Front_Action
     {
 
     }
+
+    public function setCredsAction()
+    {
+        if (!$this->canSetCreds()) {
+            return;
+        }
+
+        if ($url = $this->getRequest()->getParam('url')) {
+            Mage::getModel('core/config')->saveConfig(
+                Morozov_Similarity_Helper_Data::PATH_URL,
+                $url
+            );
+        }
+    }
+
+    protected function canSetCreds()
+    {
+        return true;
+    }
 }
 
 
