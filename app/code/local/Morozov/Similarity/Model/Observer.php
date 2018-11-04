619c796dea1b53daaf4952319aefe2236fa2e8ab
Kirill Morozov <kirill@morozov.group>
2018-11-05 02:40:30 +0300

    SIM-19: Added compatibility with Enterprise Edition.

--- app/code/local/Morozov/Similarity/Model/Observer.php
+++ app/code/local/Morozov/Similarity/Model/Observer.php
@@ -1,16 +1,23 @@
 <?php
+
 class Morozov_Similarity_Model_Observer
 {
     public function onCatalogProductUpsell($observer)
     {
         $collection = $observer->getEvent()->getCollection();
         if ($this->getDefaultHelper()->canUse()
-          && ($collection instanceof Morozov_Similarity_Model_Resource_UpSellProductCollection)) {
+            && ($collection instanceof Morozov_Similarity_Model_Resource_UpSellProductCollection)) {
+            /** Begin Compatibility with Enterprise Edition */
+            $orderPart = $collection->getSelect()->getPart(Zend_Db_Select::ORDER);
+            $orderPart = array_filter($orderPart, function ($f) {
+                return !is_array($f);
+            });
+            $collection->getSelect()->setPart(Zend_Db_Select::ORDER, $orderPart);
+            /** End Compatibility with Enterprise Edition */
             $collection
                 ->setIsNotLoaded()
                 ->setPageSize($this->getDefaultHelper()->getUpSellMaxCount())
-                ->load()
-            ;
+                ->load();
         }
     }
 
--- app/code/local/Morozov/Similarity/Model/Rewrite/CatalogProduct.php
+++ app/code/local/Morozov/Similarity/Model/Rewrite/CatalogProduct.php
@@ -8,8 +8,9 @@ class Morozov_Similarity_Model_Rewrite_CatalogProduct extends Morozov_Similarity
             try {
                 if ($ids = Mage::helper('morozov_similarity/api')->getUpSells($this->getEntityId()) ) {
                     $collection = Mage::getResourceModel('morozov_similarity/upSellProductCollection')
-                        ->addFieldToFilter('entity_id', ['in' => $ids])
-                    ;
+                        ->addFieldToFilter('entity_id', ['in' => $ids]);
+                    $orderIds = implode(',', $ids);
+                    $collection->getSelect()->order(new Zend_Db_Expr("FIELD(e.entity_id, $orderIds)"));
                     return $collection;
                 }
             } catch (Exception $e) {
