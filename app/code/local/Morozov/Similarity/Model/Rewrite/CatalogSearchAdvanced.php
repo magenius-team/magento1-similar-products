4229eff4e02a19f178416634c10c188f725be39f
anatol1984 <anatol.kravchenko@gmail.com>
2018-12-10 12:31:19 +0300

    [SIM-20] refactoring

---
+++ app/code/local/Morozov/Similarity/Model/Rewrite/CatalogSearchAdvanced.php
@@ -0,0 +1,58 @@
+<?php
+class Morozov_Similarity_Model_Rewrite_CatalogSearchAdvanced extends Mage_CatalogSearch_Model_Advanced
+{
+    public function addFilters($values)
+    {
+        try {
+            parent::addFilters($values);
+            if ($similar = $this->getRequestHelper()->getSimilar()) {
+                $this->addSimilarFilters($similar);
+            }
+        } catch (Mage_Core_Exception $e) {
+            if (($similar = $this->getRequestHelper()->getSimilar()) && $this->detectTermsNotSpecifiedMsg($e->getMessage())) {
+                $this->addSimilarFilters($similar);
+            } else {
+                throw $e;
+            }
+        }
+        return $this;
+    }
+
+    protected function detectTermsNotSpecifiedMsg($message)
+    {
+        $res = stristr($message, Mage::helper('catalogsearch')->__('Please specify at least one search term.'));
+        return $res;
+    }
+
+    protected function addSimilarFilters($similar)
+    {
+        $ids = [];
+        try {
+            $ids = @$this->getApiHelper()->getUpSells((int)$similar);
+            $this->getProductCollection()
+                ->addFieldToFilter('entity_id', ['in' => $ids])
+            ;
+        } catch (Exception $e) {
+            $this->getDefaultHelper()->log('Advanced Search: ' . $e->getMessage());
+        }
+        if (!$ids) {
+            throw new Mage_Core_Exception("Couldn't get similar products from the service..");
+        }
+
+    }
+
+    protected function getRequestHelper()
+    {
+        return Mage::helper('morozov_similarity/request');
+    }
+
+    protected function getApiHelper()
+    {
+        return Mage::helper('morozov_similarity/api');
+    }
+
+    protected function getDefaultHelper()
+    {
+        return Mage::helper('morozov_similarity');
+    }
+}
